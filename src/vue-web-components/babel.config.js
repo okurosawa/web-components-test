85ebf717c34cfc9191eaf57197c8ac4f3be4e723
okurosawa <okurosawa@bizmates.jp>
2021-11-24 05:12:12 +0000

    Change buld command to use babel.

--- src/vue-web-components/babel.config.js
+++ src/vue-web-components/babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+  presets: ["@vue/app"],
+  plugins: [
+    "transform-custom-element-classes",
+    "transform-es2015-shorthand-properties",
+    "transform-es2015-template-literals",
+    "@babel/plugin-transform-arrow-functions",
+    "@babel/plugin-transform-regenerator",
+  ],
+};
